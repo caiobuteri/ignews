c3cf7d9f4ec67e12dd9c0e8ea579171b3bcb5272
caio.buteri <caio.buteri@virtuspay.com.br>
2021-12-17 08:14:51 -0300

    first unit tests

---
+++ src/components/SignInButton/SigninButton.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react'
+import { SignInButton } from '.';
+
+jest.mock('next-auth/client', () => {
+  return {
+    useSession() {
+      return [null, false]
+    }
+  }
+})
+
+describe('SigninButton compoent', () => {
+  
+  it('renders correctly', () => {
+    render(
+      <SignInButton />
+    )
+  
+    expect(screen.getByText("Home")).toBeInTheDocument()
+    expect(screen.getByText("Posts")).toBeInTheDocument()
+  });
+});
