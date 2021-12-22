12ed4912f0fbcfe0331cde030dbf212fda32dc26
caio.buteri <caio.buteri@virtuspay.com.br>
2021-12-21 22:05:28 -0300

    tests implementations

--- src/components/SignInButton/SigninButton.test.tsx
+++ src/components/SignInButton/SigninButton.test.tsx
@@ -1,22 +1,33 @@
+import { useSession } from 'next-auth/client';
+
+import { mocked } from 'ts-jest/utils';
+
 import { render, screen } from '@testing-library/react'
 import { SignInButton } from '.';
 
-jest.mock('next-auth/client', () => {
-  return {
-    useSession() {
-      return [null, false]
-    }
-  }
-})
+jest.mock('next-auth/client')
 
 describe('SigninButton compoent', () => {
+  it('renders correctly when user is not authenticated', () => {
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce([null, false]);
+
+    render(<SignInButton />)
   
-  it('renders correctly', () => {
-    render(
-      <SignInButton />
-    )
-  
-    expect(screen.getByText("Home")).toBeInTheDocument()
-    expect(screen.getByText("Posts")).toBeInTheDocument()
+    expect(screen.getByText("Sign in with Github")).toBeInTheDocument()
+  });
+
+  it('renders correctly when user is authenticated', () => {
+    const useSessionMocked = mocked(useSession);
+
+    useSessionMocked.mockReturnValueOnce([
+      { user: { name: 'Caio Buteri', email: 'caiobuteri10@gmail.com' }, expires: 'fake-expires' },
+      false
+    ]);
+    
+    render(<SignInButton />);
+
+    expect(screen.getByText("Caio Buteri")).toBeInTheDocument()
   });
 });
