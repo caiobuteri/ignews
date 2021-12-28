fbbd0649daf3d1ee59a3e290c9fa29db927057ee
caio.buteri <caio.buteri@virtuspay.com.br>
2021-12-28 10:21:24 -0300

    preview test fixed

--- src/tests/pages/Preview.test.tsx
+++ src/tests/pages/Preview.test.tsx
@@ -7,22 +7,22 @@ import { useSession } from "next-auth/client";
 import { useRouter } from "next/router";
 
 const post = {
-  slug: 'my-new-post',
-  title: 'My new post',
-  updatedAt: '10 de Abril',
-  content: '<p>Post excerpt</p>'
-}
+  slug: "my-new-post",
+  title: "My new post",
+  updatedAt: "10 de Abril",
+  content: "<p>Post excerpt</p>",
+};
 
+jest.mock("../../services/prismic");
+jest.mock("next-auth/client");
+jest.mock("next/router");
 
-jest.mock('../../services/prismic');
-jest.mock('next-auth/client')
-
-describe('Post preview page', () => {
+describe("Post preview page", () => {
   it("renders correctly", () => {
     const useSessionMocked = mocked(useSession);
     useSessionMocked.mockReturnValueOnce([null, false]);
-    
-    render(<Post post={post} />)
+
+    render(<Post post={post} />);
 
     expect(screen.getByText("My new post")).toBeInTheDocument();
     expect(screen.getByText("Post excerpt")).toBeInTheDocument();
@@ -35,24 +35,23 @@ describe('Post preview page', () => {
     const pushMock = jest.fn();
 
     useSessionMocked.mockReturnValueOnce([
-      { activeSubscription: 'fake-active-subscription' },
-      false
+      { activeSubscription: "fake-active-subscription" },
+      false,
     ] as any);
 
     useRouterMocked.mockReturnValueOnce({
-      push: pushMock  
+      push: pushMock,
     } as any);
 
-    render(<Post post={post} />)
+    render(<Post post={post} />);
 
-    expect(pushMock).toHaveBeenCalledWith("/posts/my-new-post")
+    expect(pushMock).toHaveBeenCalledWith("/posts/my-new-post");
   });
 
   // it("loads initial data", async () => {
   //   const getSessionMocked = mocked(getSession);
   //   const getPrismicClientMocked = mocked(getPrismicClient);
 
-    
   //   getPrismicClientMocked.mockReturnValueOnce({
   //     getByUID: jest.fn().mockResolvedValueOnce({
   //       data: {
@@ -66,11 +65,11 @@ describe('Post preview page', () => {
   //       last_publication_date: '04-01-2021'
   //     })
   //   } as any);
-    
+
   //   getSessionMocked.mockReturnValueOnce({
   //     activeSubscription: 'fake-active-subscription'
   //   } as any);
-    
+
   //   const response = await getServerSideProps({
   //     params: {
   //       slug: 'my-new-post'
